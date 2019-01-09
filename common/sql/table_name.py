1733be2d25768fe457f9e1bf93590ff56b03ae40
li2chen <chenli.a0907@gmail.com>
2019-01-09 11:03:26 +0800

    多写了括号

--- common/sql/table_name.py
+++ common/sql/table_name.py
@@ -8,7 +8,7 @@ def table_name(key_name=None, key_name2=None, key=None):
 	if key_name and not key_name2:
 		sql = f'select *  from table_name where key_name ={key_name}'
 	elif key_name2 and not key_name:
-		sql = f'select *  from table_name where key_name2 = {key_name2})'
+		sql = f'select *  from table_name where key_name2 = {key_name2}'
 	else:
 		sql = f'select *  from table_name where (table_name = {table_name} and key_name2 ={key_name2})'
 	result = exec_sql(sql=sql, key=key)
