bf2f07176198e752df0cc15988754f888388d7e1
li2chen <chenli.a0907@gmail.com>
2019-02-15 12:12:47 +0800

    order -- 优化数据库查询获取结果，相同表只查询一次

--- module/order.py
+++ module/order.py
@@ -28,69 +28,75 @@ def order(headers, goods_id, num=1):
 def _create_order(headers, goods_id, num=1):
 	url = '/order/create'
 	log.info('method create_order()--> begin')
-	address = address_id(headers)
-	product_id = product(goods_id, 'id')
-	snap_id = goods(goods_id, 'snap_id')
-	# 活动商品
-	activity_id = activity(goods_id=goods_id, key='id')
-	has_activity = exists_activity(goods_id)
-	if has_activity:
-		price = int(activity(goods_id=goods_id, key='activity_min_price') * 100)
-	else:
-		price = int(product(goods_id, 'price') * 100)
-	shipping = int(logistics_info(goods(goods_id, 'logistics_template_id'), 'price') * 100)
-	order_price = price + shipping
-	# 奖励金
 	try:
-		bounty = goods_detail(goods_id=goods_id).json().get('result').get('bounty')
-		if bounty:  # 如果有奖励金，订单价格要减去奖励金；上面是单个商品的奖励金，如果多个商品，则要乘以数量
-			order_price = price * num + shipping - int(bounty) * num
-	except BaseException as e:
-		log.error('获取奖励金数据失败，下单时设置奖励金为0 --> ', e)
-		bounty = 0
-	data = {
-		"goods": [{
-			"goods_id": goods_id,
-			"product_id": product_id,
-			"price": price,
-			"num": num,
-			"snap_id": snap_id,
-			"user_info": []
-		}],
-		"shiping": shipping,
-		"order_price": order_price,
-		"addr_id": address,
-		"comments": [],
-		"ticket_nos": [],
-		"open_bounty": "true",
-		"bounty": bounty * num  # 奖励金
-
-	}
-	data_activity = {
-		"goods": [{
-			"goods_id": goods_id,
-			"product_id": product_id,
-			"price": price,
-			"num": 1,
-			"activity": {
-				"id": activity_id,
+		address = address_id(headers)
+		product_info = product(goods_id=goods_id)
+		product_id = product_info['id']
+		goods_info = goods(goods_id=goods_id)
+		snap_id = goods_info['snap_id']
+		# 活动商品
+		activity_info = activity(goods_id=goods_id)
+		if activity_info:
+			activity_id = activity_info['id']
+			price = int(activity_info['activity_min_price'] * 100)
+		else:
+			activity_id = ''
+			price = int(product_info['price'] * 100)
+		shipping = int(logistics_info(goods_info['logistics_template_id'], 'price') * 100)
+		order_price = price + shipping
+		# 奖励金
+		try:
+			bounty = goods_detail(goods_id=goods_id).json().get('result').get('bounty')
+			if bounty:  # 如果有奖励金，订单价格要减去奖励金；上面是单个商品的奖励金，如果多个商品，则要乘以数量
+				order_price = price * num + shipping - int(bounty) * num
+		except BaseException as e:
+			log.error('获取奖励金数据失败，下单时设置奖励金为0 --> ', e)
+			bounty = 0
+		data = {
+			"goods": [{
+				"goods_id": goods_id,
+				"product_id": product_id,
 				"price": price,
-				"type": "GROUPON"
-			},
-			"snap_id": snap_id,
-			"user_info": []
-		}],
-		"shiping": shipping,
-		"order_price": order_price,
-		"addr_id": address,
-		"comments": [],
-		"ticket_nos": [],
-		"open_bounty": "true",
-		"bounty": bounty * num  # 奖励金，要根据数量翻倍
-	}
-	if not has_activity:
-		resp = req(method='post', url=host + url, headers=headers, json=data)
-	else:
-		resp = req(method='post', url=host + url, headers=headers, json=data_activity)
-	log.info('method create_order()--> end')
-	return resp
+				"num": num,
+				"snap_id": snap_id,
+				"user_info": []
+			}],
+			"shiping": shipping,
+			"order_price": order_price,
+			"addr_id": address,
+			"comments": [],
+			"ticket_nos": [],
+			"open_bounty": "true",
+			"bounty": bounty * num  # 奖励金
+
+		}
+		data_activity = {
+			"goods": [{
+				"goods_id": goods_id,
+				"product_id": product_id,
+				"price": price,
+				"num": 1,
+				"activity": {
+					"id": activity_id,
+					"price": price,
+					"type": "GROUPON"
+				},
+				"snap_id": snap_id,
+				"user_info": []
+			}],
+			"shiping": shipping,
+			"order_price": order_price,
+			"addr_id": address,
+			"comments": [],
+			"ticket_nos": [],
+			"open_bounty": "true",
+			"bounty": bounty * num  # 奖励金，要根据数量翻倍
+		}
+		if not activity_id:
+			resp = req(method='post', url=host + url, headers=headers, json=data)
+		else:
+			resp = req(method='post', url=host + url, headers=headers, json=data_activity)
+		log.info('method create_order()--> end')
+		return resp
+	except Exception as e:
+		log.error('create_order() failed', e)
