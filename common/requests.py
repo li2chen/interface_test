0b6554202707aab43281f36a6462454a11e49159
li2chen <chenli.a0907@gmail.com>
2019-03-01 15:36:37 +0800

    run.py -- test goods.py

--- common/requests.py
+++ common/requests.py
@@ -9,19 +9,18 @@ log = Logger('REQUEST_LOG').log()
 def request(method, url, headers=None, params=None, data=None, json=None, verify=None):
 	resp = None
 	try:
+		log.info(f'方法 -> {method}, url -> {url}; 请求头 -> {headers}, 参数 -> {data}{json}{params}'.replace('None', ''))
 		if method.upper() == 'POST':
-			log.info(f'方法 --> POST , url --> {url} ; 请求头 --> {headers} , 参数 --> {data} ; or {json}')
 			resp = requests.post(url=url, headers=headers, data=data, json=json, verify=verify)
-			log.info(f'请求结果 --> {resp.text}')
+			log.info(f'请求结果 -> {resp.text}')
 		elif method.upper() == 'GET':
-			log.info(f'方法 --> GET ; url --> {url} ; 请求头 --> {headers} ; 参数 --> {params}')
 			resp = requests.get(url=url, headers=headers, params=params, verify=verify)
-			log.info(f'请求结果 --> {resp.text}')
+			log.info(f'请求结果 -> {resp.text}')
 		else:
 			log.error(f'method should be get or post, but {method}')
 		return resp
 	except BaseException as e:
-		log.error('请求失败，msg -->', e)
+		log.error('请求失败，msg ->', e)
 
 
 def __get_host(index='url', item='host'):
